af1836dbb938b2c8c2fc3a72c2cd59b167eef6e1
Longnguyen2006 <71785663+Longnguyen2006@users.noreply.github.com>
2022-01-15 14:15:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,42 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 import requests
 from bs4 import BeautifulSoup
 
-
 def get_news():
     list_news = []
-    r = requests.get("https://www.nbcnews.com/")
+    r = requests.get('https://www.nbcnews.com/')
     soup = BeautifulSoup(r.text, 'html.parser')
     mydivs = soup.find_all("h2", {"class": "tease-card__headline tease-card__title relative"})
 
-    for new in mydivs:
+    # print(len(mydivs))
+    for news in mydivs:
         newdict = {}
-        newdict["link"] = new.a.get("href")
-        newdict["title"] = new.a.get("title")
+        newdict["link"] = news.a.get("href")
+        newdict["title"] = news.a.get("title")
+        #print(news.a.get("href"))
         list_news.append(newdict)
 
     return list_news
 
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'xin chao {update.effective_user.first_name}')
 
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 def news(update: Update, context: CallbackContext) -> None:
-    data = get_news()
-    str1 = " "
+     data = get_news()
+     for item in data:
+            update.message.reply_text(f'{item["link"]}')
 
-    for item in data:
-        str1 += item["title"] + "\n"
-    update.message.reply_text(f'{str1}')
 
 
 
 updater = Updater('5045399918:AAFiASiKPSt0B24socl9_Es557yW3ncPSCo')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
 updater.dispatcher.add_handler(CommandHandler('news', news))
 
 updater.start_polling()
 updater.idle()
+
+
